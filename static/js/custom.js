f5747c05a6b5331577381ac135eb137182cdfa7c
JaganNS10 <ns.jagansenthilkumar@gmail.com>
2026-01-27 15:06:44 +0530

    'NWIPL'

--- static/js/custom.js
+++ static/js/custom.js
@@ -68,7 +68,53 @@
       }, 300);
     }
 });
-    
+
+  // $('.owl-service-item').owlCarousel({
+  //   items:4,
+  //   loop:true,
+  //   dots: true,
+  //   nav: false,
+  //   autoplay: true,
+  //   margin:15,
+  //   responsive:{
+  //     0:{
+  //       items:1
+  //     },
+  //     600:{
+  //       items:2
+  //     },
+  //     1000:{
+  //       items:4
+  //     }
+  //   }
+  // })
+
+  $(document).ready(function () {
+  $('.owl-service-item').owlCarousel({
+    items: 4,
+    loop: true,                // infinite slideshow
+    margin: 15,
+    autoplay: true,            // auto slide
+    autoplayTimeout: 3000,     // 3 seconds
+    autoplayHoverPause: true,  // pause on hover
+    dots: true,                // pagination dots
+    nav: false,                // no arrows
+    smartSpeed: 800,           // smooth transition
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+});
+
+
   })(window.jQuery);
 
 
